4f70b29d3bb83a95f07a9999f136e78f9f2db78a
Zhenyuan Shen <szywind@163.com>
2018-05-09 13:53:50 +0800

    fix a bug: null

--- app/src/main/java/com/application/cool/history/models/State.java
+++ app/src/main/java/com/application/cool/history/models/State.java
@@ -1,10 +1,11 @@
 package com.application.cool.history.models;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by Zhenyuan Shen on 5/6/18.
  */
 public class State {
-    public static Set<String> currentSubscribeTopics;
+    public static Set<String> currentSubscribeTopics = new HashSet<String>();
 }
