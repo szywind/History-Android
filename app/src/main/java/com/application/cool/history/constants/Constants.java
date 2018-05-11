12ba139bcb928d9094ce73f6b8cb093c3f3fe8c1
Zhenyuan Shen <szywind@163.com>
2018-05-11 09:13:41 +0800

    get address from location

--- app/src/main/java/com/application/cool/history/constants/Constants.java
+++ app/src/main/java/com/application/cool/history/constants/Constants.java
@@ -12,7 +12,7 @@ public interface Constants {
         int defaultUsernameLimit = 20;
         int defaultSmsCodeLength = 6;
         int defaultPasswordLimit = 8;
-        String defaultString = "None";
+        String defaultString = "";
     }
 
     /*************************
--- app/src/main/java/com/application/cool/history/managers/LocationManager.java
+++ app/src/main/java/com/application/cool/history/managers/LocationManager.java
@@ -1,9 +1,18 @@
 package com.application.cool.history.managers;
 
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.text.TextUtils;
 
+import com.application.cool.history.constants.Constants;
 import com.application.cool.history.util.SimpleLocation;
 import com.avos.avoscloud.AVGeoPoint;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by Zhenyuan Shen on 5/6/18.
@@ -35,5 +44,31 @@ public class LocationManager {
 
     static public boolean isInvalid(AVGeoPoint loc) {
         return loc == null || (loc.getLatitude() ==0 && loc.getLongitude() == 0);
+    }
+
+    public String getAddressFromLoc(SimpleLocation loc){
+        String address = Constants.Default.defaultString;
+        // if we can't access the location yet
+        if (!loc.hasLocationEnabled()) {
+            // ask the user to enable location access
+            SimpleLocation.openSettings(context);
+        }
+        SimpleLocation.Point pt = loc.getPosition();
+
+        Geocoder geoCoder = new Geocoder(context, Locale.getDefault());
+//        StringBuilder builder = new StringBuilder();
+        List<String> addressStr = new ArrayList<>();
+        try {
+            List<Address> addressDict = geoCoder.getFromLocation(pt.latitude, pt.longitude, 1);
+            if(!address.isEmpty()) {
+
+                int maxLineIndex = addressDict.get(0).getMaxAddressLineIndex();
+                for (int i = 0; i < Math.max(1,maxLineIndex); i++) {
+                    addressStr.add(addressDict.get(0).getAddressLine(i));
+                }
+                address = TextUtils.join(" ", addressStr); //This is the complete address.
+            }
+        } catch (IOException | NullPointerException ignored) {}
+        return address;
     }
 }
